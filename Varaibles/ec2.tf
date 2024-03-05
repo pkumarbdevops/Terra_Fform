97218391440c11b1971f757454cb2bb799f1a7ee
Praveen Kumar Devops <pkumarb.devops@gmail.com>
2024-03-05 16:44:51 +0530

    Terraform

--- Varaibles/ec2.tf
+++ Varaibles/ec2.tf
@@ -1,4 +1,9 @@
+#  resource "aws_instance" "my-wish-i-can-decide" {
+#      ami = "ami-0f3c7d07486cad139" #devops-practice in us-east-1
+#      instance_type = "t2.micro"
+#  }
+
  resource "aws_instance" "my-wish-i-can-decide" {
-     ami = "ami-0f3c7d07486cad139" #devops-practice in us-east-1
-     instance_type = "t2.micro"
+     ami = var.ami_id #devops-practice in us-east-1
+     instance_type = var.instance_type
  }
---
+++ Varaibles/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {
+  type = string # this is the data type
+  default = "ami-0f3c7d07486cad139" # this is the default value ( #devops-practice in us-east-1)
+}
+
+variable "instance_type" {
+  # type = string # this is the data type
+  default = t2.micro
+}
