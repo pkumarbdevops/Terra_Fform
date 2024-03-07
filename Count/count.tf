1259bc8b346bd0a31a49c2099746a352192533d6
Praveen Kumar Devops <pkumarb.devops@gmail.com>
2024-03-07 19:28:05 +0530

    Count

--- Count/count.tf
+++ Count/count.tf
@@ -2,6 +2,10 @@ resource "aws_instance" "count" {
   count = 10
   ami = var.ami_id #devops-practice in us-east-1
   instance_type = "t2.micro"
+  tags = {
+    Name = var.instance_names[count.index]
   }
+
+}
 
 # MongoDB Cart Catalogue User Redis MySQL RabbitMQ Shipping Payment Web
--- Count/variables.tf
+++ Count/variables.tf
@@ -2,3 +2,8 @@ variable "ami_id" {
   default = "ami-0f3c7d07486cad139"
 }
 
+variable "instance_names" {
+  type = list
+  default = ["MongoDB", "Cart", "Catalogue", "User" , "Redis", "MySQL", "RabbitMQ", "Shipping", "Payment", "Web"]
+}
+
