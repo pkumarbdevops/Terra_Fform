f387e911016d58691e5d2c9cccbfcf61e2112c09
Praveen Kumar Devops <pkumarb.devops@gmail.com>
2024-03-07 19:39:16 +0530

    Count

--- Count/count.tf
+++ Count/count.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "count" {
   count = 10
   ami = var.ami_id #devops-practice in us-east-1
-  instance_type = "t2.micro"
+  #instance_type = "t2.micro"
+  instance_type = var.instance_names[count.index] == "MongoDB" || var.instance_names[count.index] == "MySQL" ? "t3.medium" : "t2.micro"
   tags = {
     Name = var.instance_names[count.index]
   }
