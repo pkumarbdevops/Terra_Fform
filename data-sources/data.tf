e66cf67c78a59882d67ce02a4e582a2d066e1ce4
Praveen Kumar Devops <pkumarb.devops@gmail.com>
2024-03-11 11:42:01 +0530

    datasource

---
+++ data-sources/data.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "ami_id" {
+    most_recent = true
+    owners = ["amazon"] #you can give AWS Account ID
+
+    filter {
+        name   = "name"
+        values = ["amzn2-ami-kernel-5.10-hvm-*.0-x86_64-gp2"]
+    }
+}
+
+output "ami_id" {
+  value = data.aws_ami.ami_id.id
+}
