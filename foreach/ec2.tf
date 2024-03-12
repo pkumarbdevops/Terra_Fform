d6b68b75f4ff2cf2c9c1433f6bc02a5559ab5c86
Praveen Kumar Devops <pkumarb.devops@gmail.com>
2024-03-12 12:10:10 +0530

    foreach

--- foreach/ec2.tf
+++ foreach/ec2.tf
@@ -7,18 +7,18 @@ resource "aws_instance" "roboshop" {
   }
   }
 
-output "aws_instance_info" {
-   value = aws_instance.roboshop
- }
+# output "aws_instance_info" {
+#    value = aws_instance.roboshop
+#  }
 
 #if web give public IP else you give private IP
-# resource "aws_route53_record" "www" {
-#   for_each = aws_instance.roboshop
-#   zone_id = var.zone_id
-#   name    = "${each.key}.${var.domain}"
-#   type    = "A"
-#   ttl     = 1
-#   records = [ each.key == "Web" ? each.value.public_ip : each.value.private_ip ]
-# }
+resource "aws_route53_record" "www" {
+  for_each = aws_instance.roboshop
+  zone_id = var.zone_id
+  name    = "${each.key}.${var.domain}"
+  type    = "A"
+  ttl     = 1
+  records = [ each.key == "Web" ? each.value.public_ip : each.value.private_ip ]
+}
 
 
